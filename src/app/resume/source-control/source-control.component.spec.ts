c00c78d5d5dea8cd394da455e8a5203a16410ba1
James Zimowsky <jameszimo@gmail.com>
2022-12-23 15:35:46 -0800

    Change repo links and images to be github

--- src/app/resume/source-control/source-control.component.spec.ts
+++ src/app/resume/source-control/source-control.component.spec.ts
@@ -5,6 +5,7 @@ import {SourceControlComponent} from './source-control.component';
 describe('SourceControlComponent', () => {
   let component: SourceControlComponent;
   let fixture: ComponentFixture<SourceControlComponent>;
+  let compiled: any;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -17,9 +18,27 @@ describe('SourceControlComponent', () => {
     fixture = TestBed.createComponent(SourceControlComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    compiled = fixture.nativeElement;
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should show a gitHub repo URL', () => {
+    expect(compiled.querySelector('#source-control-url').textContent).toEqual(
+        'https://github.com/jameszimo/resume-angular'
+    );
+  });
+
+  it('should have a link to a GitHub Repo', () => {
+    const gitHubUrl = compiled.querySelector('.gitHub').querySelector('a').href
+    expect(gitHubUrl).toEqual('https://github.com/jameszimo/resume-angular');
+  });
+
+  it('should have the GitHub logo', () => {
+    const gitHubLogoUrl = compiled.querySelector('.gitHub').querySelector('img').src
+    const path = new URL(gitHubLogoUrl).pathname;
+    expect(path).toEqual('/assets/images/github.png');
   });
 });
