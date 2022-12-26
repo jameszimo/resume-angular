3821ca27ee0054fa4ba3efb9596264ea01ffbacd
James Zimowsky <jameszimo@gmail.com>
2022-12-25 21:29:32 -0800

    Another try to redirect http to https

--- server.js
+++ server.js
@@ -3,6 +3,14 @@ const path = require('path');
 
 const app = express();
 
+app.get('*',function(req,res,next){
+  if(req.headers['x-forwarded-proto']!='https' && process.env.NODE_ENV === 'production') {
+    res.redirect('https://james.zimowsky.net');
+  } else {
+    next();
+  }
+});
+
 app.use(express.static(__dirname + '/dist'));
 app.get('/*', function(req, res) {
   res.sendFile(path.join(__dirname + '/dist/index.html'));
