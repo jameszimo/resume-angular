0e3ebea73dbd2502d6bd2d5a97717e6a563c3f25
James Zimowsky <jameszimo@gmail.com>
2022-12-25 21:10:47 -0800

    Revert "trying out a method to redirect to ssl" - Didn't seem to work.

--- server.js
+++ server.js
@@ -1,18 +1,7 @@
-const express = require('express'), env = process.env.NODE_ENV || 'development';
+const express = require('express');
 const path = require('path');
 
-var forceSSL = function (req, res, next) {
-  if(req.headers['x-forward-proto'] !== 'https') {
-    return res.redirect(['https://', req.get('host'), req.url].join(''));
-  }
-  return next();
-}
-
 const app = express();
-
-if(env === 'production') {
-  app.use(forceSSL);
-}
 
 app.use(express.static(__dirname + '/dist'));
 app.get('/*', function(req, res) {
