bbdcddd597dbe69406335c751c08361e6cbeb541
James Zimowsky <jameszimo@gmail.com>
2022-12-27 11:34:30 -0800

    More redirect experiments

--- server.js
+++ server.js
@@ -1,18 +1,30 @@
 const express = require('express');
 const path = require('path');
+const env = process.env.NODE_ENV || 'undefined';
+
+console.log('env: ' + env)
 
 const app = express();
 
-app.get('*',function(req,res,next){
-  if(req.headers['x-forwarded-proto']!='https' && process.env.NODE_ENV === 'production') {
-    res.redirect('https://james.zimowsky.net');
-  } else {
+app.get('*', function (req, res, next) {
+
+  if (env !== 'production') {
     next();
+
+  } else if ((req.headers['x-forwarded-proto'] || '') === 'https') {
+    next();
+
+  } else if (req.method === 'GET' || req.method === 'HEAD') {
+    const host = req.headers['x-forwarded-host'] || req.headers.host;
+    res.redirect(301, `https://${host}${req.originalUrl}`);
+
+  } else {
+    res.status(403).send('This server requires an HTTPS connection.');
   }
 });
 
 app.use(express.static(__dirname + '/dist'));
-app.get('/*', function(req, res) {
+app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
 
